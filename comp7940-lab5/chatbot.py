642f1ea36e6c94bc37392ff49a301a1114681377
MarsTan <marstan@MarsTandeMacBook-Pro.local>
2024-03-08 19:37:43 +0800

    lab5

--- comp7940-lab5/chatbot.py
+++ comp7940-lab5/chatbot.py
@@ -1,87 +1,82 @@
-from telegram import Update
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
+## this file is based on version 13.7 of python telegram chatbot
+## and version 1.26.18 of urllib3
+## chatbot.py
+import os
+import json
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+# The messageHandler is used for all message updates
 import configparser
 import logging
-#import redis
-from ChatGPT_HKBU import HKBU_ChatGPT
 
-#global redis1
-global chatgpt
+#import subprocess
+#import redis_server
+#subprocess.Popen([redis_server.REDIS_SERVER_PATH])
+
+#from gptbot import HKBU_GPT
+import requests
+
+
 def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
+    print(config['TELEGRAM']['ACCESS_TOKEN'])
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    #updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    #global redis1
-    #redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-    #                     password=(config['REDIS']['PASSWORD']),
-    #                     port=(config['REDIS']['REDISPORT']))
-    # You can set this logging module, so you will know when
-    # and why things do not work as expected Meanwhile, update your config.ini as:
+    # You can set this logging module,
+    # so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    # register a dispatcher to handle message: here we register an echo dispatcher
-    #echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    #dispatcher.add_handler(echo_handler)
-
+    # register a dispatcher to handle message:
+    # here we register an echo dispatcher
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
     global chatgpt
-    chatgpt = HKBU_ChatGPT(config)
-    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    #chatgpt = HKBU_GPT(config)
+    chatgpt = HKBU_GPT()
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
-
-    # 注册错误处理程序
-    dispatcher.add_error_handler(error_handler)
-
-    # on different commands - answer in Telegram
-    #dispatcher.add_handler(CommandHandler("add", add))
-    #dispatcher.add_handler(CommandHandler("help", help_command))
     # To start the bot:
     updater.start_polling()
     updater.idle()
-
-#def echo(update, context):
-#    reply_message = update.message.text.upper()
-#    logging.info("Update: " + str(update))
- #   logging.info("context: " + str(context))
- #   context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-#def help_command(update: Update, context: CallbackContext) -> None:
-#    """Send a message when the command /help is issued."""
-#    update.message.reply_text('Helping you helping you.')
-
-#def add(update: Update, context: CallbackContext) -> None:
-#    """Send a message when the command /add is issued."""
- #   try:
-  #      global redis1
-  #      logging.info(context.args[0])
-  #      msg = context.args[0] # /add keyword <-- this should store the keyword
-  #      redis1.incr(msg)
-#
-  #      update.message.reply_text('You have said ' + msg + ' for ' +
-  #                      redis1.get(msg).decode('UTF-8') + ' times.')
-  #  except (IndexError, ValueError):
- #       update.message.reply_text('Usage: /add <keyword>')
+    
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 
 def equiped_chatgpt(update, context):
     global chatgpt
-    try:
-        reply_message = chatgpt.submit(update.message.text)
-        logging.info("Update: " + str(update))
-        logging.info("context: " + str(context))
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " +str(update))
+    logging.info("context :" + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
-         # 提取 Response 对象中的内容并转换为字符串
-        reply_text = reply_message[0]
-        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
-    except Exception as e:
-        logging.error("An error occurred while processing the message: %s", str(e))
-
-
-def error_handler(update, context):
-    """Log Errors caused by Updates."""
-    logging.error("Exception while handling an update:", exc_info=context.error)
-
-
+class HKBU_GPT():
+    #def __init__(self, config='./config.ini'):
+    def __init__(self, config='./config.ini'):
+        if type(config) == str:
+            self.config = configparser.ConfigParser()
+            self.config.read(config)
+        elif type(config) == configparser.ConfigParser:
+            self.config = config
+        #pass
+    
+    def submit(self, message):
+        conversation = [{"role": "user", "content": message}]
+        url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
+        #url = (os.environ['BASICURL']) + "/deployments/" + (os.environ['MODELNAME']) + "/chat/completions/?api-version=" + (os.environ['APIVERSION'])
+        headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        #headers = { 'Content-Type': 'application/json', 'api-key': (os.environ['GPT_ACCESS_TOKEN']) }
+        payload = { 'messages': conversation }
+        response = requests.post(url, json=payload, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['choices'][0]['message']['content']
+        else:
+            return 'Error:', response
+        
 if __name__ == '__main__':
     main()
